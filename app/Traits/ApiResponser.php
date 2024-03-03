c7a134d3cbbc19bc643595f5660ba91f0d72ffc3
evecat <bartolaba.ruzhanne96@gmail.com>
2024-03-03 12:40:56 +0800

    first commit by Bartolaba

---
+++ app/Traits/ApiResponser.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\Response;
+
+trait ApiResponser
+{
+    /**
+     * Build success response
+     * @param string|array $data
+     * @param int $code
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function successResponse($data, $code = Response::HTTP_OK)
+    {
+        return response()->json(['data' => $data], $code);
+    }
+}
